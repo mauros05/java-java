f598651c9b1efe9f2bdfb8c7e086ef43a59563a6
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-04-11 11:59:00 -0600

    Vectores

---
+++ Vectores.java
@@ -0,0 +1,18 @@
+public class Vectores {
+    public static void main(String[] args){
+        int numeros[] =  new int[5];
+
+        numeros[0] = 5;
+        numeros[1] = 10;
+        numeros[2] = 15;
+        numeros[3] = 20;
+        numeros[4] = 25;
+
+        System.out.print("["+ numeros[0] + "]");
+        System.out.print("["+ numeros[1] + "]");
+        System.out.print("["+ numeros[2] + "]");
+        System.out.print("["+ numeros[3] + "]");
+        System.out.print("["+ numeros[4] + "]");
+
+    }
+}
