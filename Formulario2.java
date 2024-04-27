b985b7ae528bb9192a32c66470a6b28cfcdc1fbb
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-04-26 18:58:47 -0600

    Move formulario2

--- Formulario2.java
+++ Formulario2.java
@@ -3,4 +3,19 @@ import  java.awt.event.*;
 
 public class Formulario2 extends JFrame implements  ActionListener {
     
+    JButton button1;
+
+    public Formulario2(){
+        setLayout(null);
+        button1 = new  JButton("Cerrar")
+        button1.setBounds(300, 250, 100, 30);
+        add(button1);
+        button1.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent  e){  // Esto es  para capturar  el evento
+        if(e.getSource() == button1 ){ //metodo getSource recupera el evento del espacio de memoria
+
+        }
+    }
 }
